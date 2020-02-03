6692d56e3ef8c76f99d00eba6e6403ceda21ede4
Micah Banschick <micha.ban@gmail.com>
2020-02-03 16:09:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,29 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    Song.new 
+    @@all << self 
+  end
+  
+  def self.new_by_name(name)
+    Song.new(name)
+    @name = name 
+  end 
+  
+  def self.create_by_name(name)
+    Song.new(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.find_by_name(name)
+    @@all.each{|song| return song if song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if @@all.any?{|song| song.name == name} == true 
+      return @@all.each
+  
 end
