1b97971552182d3ed5f9c9d04df140708056a7b5
Micah Banschick <micha.ban@gmail.com>
2020-02-03 16:47:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     return song
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name(nom)
     song = Song.new 
-    @name = name 
+    @name = nom 
   end 
   
   def self.create_by_name(name)
